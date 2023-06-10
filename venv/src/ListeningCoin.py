f717027915a6ea4ffc8bfbc0e5eed7e9aa42462a
luffy154 <1548476679@qq.com>
2023-06-10 20:20:35 +0800

    uniswap新池子监控

--- venv/src/ListeningCoin.py
+++ venv/src/ListeningCoin.py
@@ -4,7 +4,7 @@ import time
 
 # 以太坊新池子监听
 # 连接到以太坊节点
-web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/1e7dd9bcc537448fa50a14f60ed3cbec'))
+web3 = Web3(Web3.HTTPProvider('https://proud-wider-sponge.discover.quiknode.pro/700cf84ed019777ba23c95c754089411ce3de184/'))
 
 # Uniswap 工厂合约地址和 ABI
 uniswap_factory_address = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
@@ -54,9 +54,10 @@ event_filter = uniswap_factory_contract.events.PairCreated.create_filter(fromBlo
 # 处理事件回调函数
 def handle_event(event):
     # 在这里处理新交易对事件
-    pair_address = event['args']['pair']
-    print(json.dumps(event))
-    print(f'New pair created: {pair_address}')
+    # {'args': AttributeDict({'token0': '0xbC1363a40aF7Ee8B266ceDf54Df396b2E7aE2f12', 'token1': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 'pair': '0x1A66be956F5BF3AD4Eaa76EB030e938687BD3eDa', '': 196947}), 'event': 'PairCreated', 'logIndex': 227, 'transactionIndex': 127, 'transactionHash': HexBytes('0xe0ea00bd68cc300231a175daba3f0da91b8021c08bfc7d7beb89de94ca98d680'), 'address': '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', 'blockHash': HexBytes('0xf2bb52d55f3c80765a6f7b44f4ffa53835968326d68f4c2368a5399e2765435a'), 'blockNumber': 17449134}
+    token0_address = event['args']['token0']
+    print(f'New pair created: {token0_address}')
+
 
 
 # 监听事件
